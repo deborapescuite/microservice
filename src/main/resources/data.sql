ff035a70b139213b61c6418d503090bdcdd03f8d
Debora <deborapescuite@gmail.com>
2018-06-03 21:27:18 -0300

    add product

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-insert into tbl_product (name,description,unit_value, quantity) values('pencil','blue pencil',25.5,1)
+insert into tbl_product (name,description,unit_value, quantity) values('Pencil','blue pencil',25.5,1)
+insert into tbl_product (name,description,unit_value, quantity) values('Plastic folder','Red plastic folder',10.0,1)
 
 insert into tbl_user (name,create_date,user_type) values('ADMIN',CURRENT_TIMESTAMP(),0)
 insert into tbl_user (name,create_date,user_type) values('CUSTOMER_A',CURRENT_TIMESTAMP(),1)
