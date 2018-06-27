bc09260322f312caad871d4b88e8e62a779e8ae4
Debora <deborapescuite@gmail.com>
2018-06-26 21:53:58 -0300

    add security

---
+++ src/main/java/com/dpgb/microservice/exception/ExpiredTokenException.java
@@ -0,0 +1,4 @@
+package com.dpgb.microservice.exception;
+
+public class ExpiredTokenException {
+}
---
+++ src/main/java/com/dpgb/microservice/security/JwtTokenFilterConfigurer.java
@@ -0,0 +1,4 @@
+package com.dpgb.microservice.security;
+
+public class JwtTokenFilterConfigurer {
+}
