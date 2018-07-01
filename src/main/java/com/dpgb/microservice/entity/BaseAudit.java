79da82dd8bc8f0b5da430b8d408d3a8a87f6ccde
Debora <deborapescuite@gmail.com>
2018-06-30 23:23:16 -0300

    added created by and modified by

--- src/main/java/com/dpgb/microservice/entity/BaseAudit.java
+++ src/main/java/com/dpgb/microservice/entity/BaseAudit.java
@@ -1,6 +1,8 @@
 package com.dpgb.microservice.entity;
 
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
@@ -16,6 +18,11 @@ public class BaseAudit {
     @Temporal(TemporalType.TIMESTAMP)
     private Date modifiedDate;
 
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedBy
+    private String modifiedBy;
 
     public Date getCreatedDate() {
         return createdDate;
@@ -33,6 +40,22 @@ public class BaseAudit {
         this.modifiedDate = modifiedDate;
     }
 
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
     @PrePersist
     public void prePersist() {
         setCreatedDate(new Date());
@@ -42,4 +65,5 @@ public class BaseAudit {
     public void preUpdate() {
         setModifiedDate(new Date());
     }
+
 }
